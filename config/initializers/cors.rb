cc5a795a6913b1e513d05ecc8efe833fb4c12e81
Linwood Lassiter <lin1ood@embarqmail.comx>
2017-09-18 21:12:23 -0400

    JWT in place and working for Users

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,22 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ['https://gizmo-blogger-backend.herokuapp.com', 'localhost:3001']
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # methods: [:get, :options, :head]
+    end
+    allow do
+      origins '*'
+
+      resource '*',
+        headers: :any,
+        methods: [:get, :options, :head]
+        # methods: [:get, :options, :head]
+      end
+end
